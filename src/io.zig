cd1f75727852ee89c8f3f4bd9edb0384c13ea6e9
Michael Odden <me@michaelodden.com>
2021-10-17 15:10:56 +0200

    Fixed io.zig-tests

--- src/io.zig
+++ src/io.zig
@@ -19,7 +19,7 @@ pub fn readFileRawRel(dir: std.fs.Dir, path: []const u8, target_buf: []u8) !usiz
 test "readFileRaw" {
     var buf = utils.initBoundedArray(u8, 1024 * 1024);
     try testing.expect(buf.slice().len == 0);
-    try buf.resize(try readFileRaw("testdata/01-warnme/01-warnme-stats.pi", buf.buffer[0..]));
+    try buf.resize(try readFileRaw("testdata/01-warnme/01-warnme-status-ok.pi", buf.buffer[0..]));
     try testing.expect(buf.slice().len > 0);
 }
 
@@ -41,7 +41,7 @@ pub fn readFileRel(comptime T: type, comptime S: usize, dir: std.fs.Dir, path: [
 test "readFile" {
     var buf = utils.initBoundedArray(u8, 1024 * 1024);
     try testing.expect(buf.slice().len == 0);
-    try readFile(u8, buf.buffer.len, "testdata/01-warnme/01-warnme-stats.pi", &buf);
+    try readFile(u8, buf.buffer.len, "testdata/01-warnme/01-warnme-status-ok.pi", &buf);
     try testing.expect(buf.slice().len > 0);
 }
 
