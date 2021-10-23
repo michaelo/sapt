9d5e5f5f888ab73b8d301351ccb5d6b550492338
Michael Odden <me@michaelodden.com>
2021-10-23 19:25:12 +0200

    Factored out version to separate file (VERSION), updated config.zig and multibuild.sh to use this file. Updated multibuild.sh to generate .zip-archives for windows-builds.

--- multibuild.sh
+++ multibuild.sh
@@ -2,7 +2,7 @@ OUT_BASEDIR=artifacts
 mkdir -p $OUT_BASEDIR
 
 # Get version from config.zig
-VERSION=$(grep "APP_VERSION" src/config.zig | sed "s/^.*\"\(.*\)\".*$/\1/")
+VERSION=$(cat VERSION)
 
 declare -a TARGETS=("x86_64-windows-gnu" "x86_64-linux-gnu" "x86_64-macos-gnu")
 
@@ -18,10 +18,12 @@ for TARGET_FULL in "${TARGETS[@]}"; do
     cp CREDITS ${OUTDIR}
     cp xbuild/libs/${TARGET}/* ${OUTDIR}
     cd ${OUTDIR}
-    tar cfJ ../sapt-v${VERSION}-${TARGET}.tar.xz *
-    mv ../sapt-v${VERSION}-${TARGET}.tar.xz ../../
+    # TODO: If windows: create zip, otherwise .tar.xz
+    if [[ $TARGET == *"windows"* ]]; then
+        zip -9 ../../sapt-v${VERSION}-${TARGET}.zip *
+    else
+        tar cfJ ../../sapt-v${VERSION}-${TARGET}.tar.xz *
+    fi
     cd ../../..
     fi
 done
-
-# TODO: make this for windows...
---
+++ release.sh
@@ -0,0 +1,7 @@
+# Input: version (start with num)
+# Update VERSION
+# Build native and run tests
+# If successful: run all crossbuilds and grenerate archives
+# If successful: stage VERSION, commit and tag
+# If successful: TBD: push, create github-release, upload artifacts
+#                If verison is "x.y.z", it's a proper release, otherwise a prerelease
