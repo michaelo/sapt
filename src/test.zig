7c609377746eb91990775840daf2d95eaedbdc0c
Michael Odden <me@michaelodden.com>
2021-10-24 02:01:12 +0200

    Updated test.zig to include all files explicitly.

--- src/test.zig
+++ src/test.zig
@@ -1,13 +1,16 @@
 // Import all modules to be automatically tested
 // TODO: This can probably be generated compile-time
 test "link" {
+    _ = @import("argparse.zig");
+    _ = @import("config.zig");
+    _ = @import("console.zig");
+    _ = @import("httpclient.zig");
+    _ = @import("io.zig");
+    _ = @import("kvstore.zig");
     _ = @import("main.zig");
     _ = @import("parser.zig");
-    _ = @import("kvstore.zig");
-    _ = @import("io.zig");
-    _ = @import("console.zig");
-    _ = @import("argparse.zig");
+    _ = @import("pretty.zig");
+    _ = @import("threadpool.zig");
     _ = @import("types.zig");
     _ = @import("utils.zig");
-    _ = @import("httpclient.zig");
 }
