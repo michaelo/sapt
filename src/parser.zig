ad24d9a45c718b73620682d743ad4faf542ac2b9
Michael Odden <me@michaelodden.com>
2021-10-25 21:12:52 +0200

    parser.zig: removed switch on non-existent error

--- src/parser.zig
+++ src/parser.zig
@@ -241,7 +241,7 @@ pub fn expandVariablesAndFunctions(comptime S: usize, buffer: *std.BoundedArray(
 
     if(maybe_variables_sets) |variables_sets| for (variables_sets) |variables| {
         expandVariables(buffer.buffer.len, MAX_VARIABLES, buffer, &pairs, variables) catch |e| switch(e) {
-            error.NoSuchVariableFound => {}, // This is OK, as we don't know which variable_set the variable to expand may be in 
+            //error.NoSuchVariableFound => {}, // This is OK, as we don't know which variable_set the variable to expand may be in 
             else => return e
         };
     };
